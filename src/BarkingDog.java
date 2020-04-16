92220bed123ce91bb1f7a7536aa8fdcb9b768586
ryoray <praretno.wibowo@gmail.com>
2020-04-16 07:33:29 +0700

    Initial commit

---
+++ src/BarkingDog.java
@@ -0,0 +1,14 @@
+public class BarkingDog {
+
+    public static boolean shouldWakeUp (boolean barking, int hourOfDay) {
+        if (!barking) {
+            return barking;
+        } else {
+            if ((hourOfDay >=0 && hourOfDay < 8) || (hourOfDay == 23)) {
+                return barking;
+            } else {
+                return (!barking);
+            }
+        }
+    }
+}
